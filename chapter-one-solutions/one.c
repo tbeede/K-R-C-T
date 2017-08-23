06827ec8b2903bf70d2c9ee2085b304a29ecde4f
Other Human <otherhuman@Nulls-MBP.attlocal.net>
2017-08-22 23:07:20 -0400

    Added new directory for solutions; add solution to problem three

---
+++ chapter-one-solutions/one.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Exercise 1-1 of The C Programming Language
+ *
+ * Run the "hello, world" program on your system. Experiment with leaving 
+ * out parts of the program, to see what error messages you get. 
+ */
+
+int main() 
+{
+  	printf("Hello, world!\n");
+	return 0;
+}
