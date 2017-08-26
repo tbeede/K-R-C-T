b0bc84686b4606e1ceb32dce240b02132a7894c4
Other Human <otherhuman@Nulls-MBP.attlocal.net>
2017-08-26 14:01:42 -0400

    Solution for problem ten

--- chapter-one/ten.c
+++ chapter-one/ten.c
@@ -2,14 +2,47 @@
 #include <stdlib.h>
 
 /*
- * Exercise 1-10 of The C Programming Language
- *
- * Write a program to copy its input to its output, replacing each
- * tab by \t, each backspace by \b, and each backslash by \\. This
- * makes tabs and backspaces visible in an unambiguous way.
- */
+* Exercise 1-10 of The C Programming Language
+*
+* Write a program to copy its input to its output, replacing each
+* tab by \t, each backspace by \b, and each backslash by \\. This
+* makes tabs and backspaces visible in an unambiguous way.
+*/
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-	return 0;
+	int c, tab, backspace, backslash;
+
+	tab = '\t';
+	backslash = '\\';
+	backspace = '\b';
+
+	while((c = getchar()) != EOF)
+	// Note: if else threw errors without the ugly curlies
+	// replace with tabs
+	if(c == tab)
+	{
+		putchar('\\');
+		putchar('t');
+	}
+
+	// replace with backspace (doesn't seem to work; gave up)
+	else if(c == backspace)
+	{
+		putchar('\\');
+		putchar('b');
+	}
+
+	// replace with backslash
+	else if(c == backslash)
+	{
+		putchar('\\');
+		putchar('\\');
+	}
+
+	// handle everything else
+	else
+	{
+		putchar(c);
+	}
 }
