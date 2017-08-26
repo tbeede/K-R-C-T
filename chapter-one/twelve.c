7c7f902eb9e05e9a3919d41526f061a57c72f682
tbeede <otherhuman@Nulls-MBP.attlocal.net>
2017-08-26 14:58:04 -0400

    Solution for problem 12.

--- chapter-one/twelve.c
+++ chapter-one/twelve.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 
 /*
- * Exercise 1-12 of The C Programming Language
- *
- * Write a program that prints its input one word per line.
- */
+* Exercise 1-12 of The C Programming Language
+*
+* Write a program that prints its input one word per line.
+*/
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
+	int c;
+	c = getchar();
+	while (c != EOF) {
+		// Ensure it still prints one word per line even with tabs and new lines
+		if (c == ' ' || c == '\t' || c == '\n' || c == '\b')
+			putchar('\n');
+
+		else
+			putchar(c);
+			c = getchar();
+	}
 	return 0;
 }
