79ac467031dd4bd58b592c3be227811edea61133
tbeede <otherhuman@Nulls-MBP.attlocal.net>
2017-08-27 15:54:35 -0400

    Solution for problem nineteen

--- chapter-one/nineteen.c
+++ chapter-one/nineteen.c
@@ -8,59 +8,61 @@
 * Use it to write a program that reverses its input a line at a time.
 */
 
-#define MAXLENGTH 50 /* max input line size */
+#define MAXLENGTH 666 /* max input line size */
 
 /*
 * NOTE: Using function 'getline' from the book produced strange errors.
 * Stackoverflow recommended using this function name. Error no longer appears.
 */
 
-int get_line(char line[], int maxline);
-void reverses(char s[]);
+int read_line(char line[], int maxline);
+void reverses(char array[]);
 
 int main(int argc, char **argv)
 {
 	int len;            /* current line length */
 	char line[MAXLENGTH];     /* current input line */
 
-//TODO: Program does not reverse numbers or symbols
-	printf("\nInput line to reverse:\t");
-	while ((len = get_line(line, MAXLENGTH)) > 0) {
+	printf("\nInput string to reverse: ");
+	while ((len = read_line(line, MAXLENGTH)) > 0) {
 		reverses(line);
-		printf("\nThe line reversed is:\t");
+		printf("\nThe string reversed is: ");
 		printf("%s", line);
 		return 0;
 	}
 }
 
-/* get_line: read a line into s; return its length */
-int get_line(char s[], int lim)
+/* read_line: read a string into array; return string length */
+int read_line(char array[], int lim)
 {
 	int c, i;
 
 	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-		s[i] = c;
+		array[i] = c;
 
 	if (c == '\n')
-		s[i] = c;
+		array[i] = c;
 		++i;
 
-	s[i] = '\0';
+	array[i] = '\0';
 	return i;
 }
 
-/* reverses:  reverses the character string s */
-void reverses(char s[])
+/* reverses:  reverses the string array */
+void reverses(char array[])
 {
     int i, len;
+		char c;
 
     /* get length of string */
-    for (len = 0; s[len] != '\0' && s[len] != '\n'; ++len)
-        len--;
+    for (len = 0; array[len] != '\0' && array[len] != '\n'; ++len)
+        ;
 
 		/* reverse string */
     for (i = 0; i < len; ++i) {
-        s[i] = s[len - 1];
+			c = array[i];
+        array[i] = array[len - 1];
+				array[len - 1] = c;
         --len;
     }
 }
