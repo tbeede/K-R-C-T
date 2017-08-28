89545c548f0b00272789139305b4364815a97520
tbeede <otherhuman@Nulls-MBP.attlocal.net>
2017-08-28 19:55:43 -0400

    Solution for three

--- chapter-two/three.c
+++ chapter-two/three.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 /*
 * Exercise 2-3 of The C Programming Language
@@ -12,65 +11,58 @@
 
 #define MAXLENGTH 100 /* max input line size */
 
-#define YES 1
-#define NO 0
-
 int read_line(char line[], int maxline);
 int htoi(char hexstr[]);
 
 int main(int argc, char **argv)
 {
-	char line[MAXLENGTH];     /* current input line */
+  char line[MAXLENGTH];  /* current input line */
 
-	printf("\nInput hex: ");
+  printf("\nInput hex: ");
   read_line(line, MAXLENGTH);
-	printf("\nThe conversion is: %d\n", htoi(line));
+  printf("\nThe conversion is: %d\n", htoi(line));
   return 0;
 }
 
 /* read_line: read a string into array; return string length */
 int read_line(char array[], int lim)
 {
-  /* where c gets the character from i, the input */
-	int c, i;
 
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-		array[i] = c;
+  int c, i;
 
-	if (c == '\n')
-		array[i] = c;
-		++i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+  array[i] = c;
 
-	array[i] = '\0';
-	return i;
+  if (c == '\n')
+  array[i] = c;
+  ++i;
+
+  array[i] = '\0';
+  return i;
 }
 
 /* htoi:  convert s to integer */
 int htoi(char s[])
 {
-  int hexdigit,i,n, c; //inhex
+  int hex, i, n, c;
 
   n = 0;
-  //i = 0;
 
-  //inhex = YES;
+  for (i = 0; (c = s[i]) != '\0'; ++i) {
+    if (c >= '0' && c <= '9')
+      hex = c - '0';
 
- for (i = 0; (c = s[i]) != '\0'; ++i) {
-  if (c >= '0' && c <= '9')
-  hexdigit = c - '0';
+    else if (c >= 'a' && c <='f')
+      hex = c - 'a' + 10;
 
-  else if (c >= 'a' && c <='f')
-  hexdigit = c -'a' + 10;
+    else if (c >= 'A' && c <='F')
+      hex = c - 'A' + 10;
 
-  else if (c >= 'A' && c <='F')
-  hexdigit = c -'A' + 10;
+    else
+      return n;
 
-  else
-    return n;
-
-    // if(inhex == YES)
-     n = 16 * n + hexdigit;
-}
+    n = 16 * n + hex;
+  }
   return n;
 
 }
