34410fc209f5ea494811e935845a17c2dda591bc
tbeede <otherhuman@Nulls-MBP.attlocal.net>
2017-08-28 23:30:41 -0400

    Solution to problem four

--- chapter-two/four.c
+++ chapter-two/four.c
@@ -9,7 +9,7 @@
 * any character in the string s2.
 */
 
-#define MAXLINE 100 /* max input line size */
+#define MAXLINE 10 /* max input line size */
 
 int read_line(char line[], int maxline);
 void squeeze(char s1[],char s2[]);
@@ -17,50 +17,67 @@ void squeeze(char s1[],char s2[]);
 int main(int argc, char **argv)
 {
 
-	char s1[MAXLINE],s2[MAXLINE];
+	char s1[MAXLINE], s2[MAXLINE];
 
-    putchar('s');
-    putchar('1');
-		read_line(s1, MAXLINE);
+	putchar('s');
+	putchar('1');
+	read_line(s1, MAXLINE);
 
-    putchar('s');
-    putchar('2');
-		read_line(s2, MAXLINE);
+	putchar('s');
+	putchar('2');
+	read_line(s2, MAXLINE);
 
-    squeeze(s1, s2);
+	squeeze(s1, s2);
 
-    printf("\n%s\n", s1);
+	printf("\n%s\n", s1);
 }
 
 /* read_line: read a string into array; return string length */
-int read_line(char array[], int lim)
+int read_line(char s[], int lim)
 {
+	int c, i;
 
-  int c, i;
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+		if (c == '\n')
+			s[i++] = c;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-  array[i] = c;
-
-  if (c == '\n')
-  array[i] = c;
-  ++i;
-
-  array[i] = '\0';
-  return i;
+			s[i] = '\0';
+	return i;
 }
 
-void squeeze(char s1[],char s2[])
+/* delete each character in s1 that matches any character in the string s2. */
+void squeeze(char s1[], char s2[])
 {
-    int i, j, k;
-    k = 0;
+	int i, j, k;
 
-    for(i = 0; s1[i] != '\0';++i)
-    {
-        for(j = 0; (s1[i] != s2[j]) && s2[j] != '\0'; ++j)
-            ;
-        if(s2[j] == '\0')
-            s1[k++] = s1[i];
-    }
+k = 0;
+	for (i = j = 0; s1[i] != '\0'; i++)
+		if (s1[i] != s2[j])
+			s1[k++] = s1[i];
 
-    s1[k] = '\0';
+	// j = 0;
+	// k = 0;
+	// for (i = 0; s1[i] != '\0'; i++)
+	// 	if ((s1[i] != s2[j]) && (s2[j] == '\0')){
+	// 		//k = s2[j] - s1[i];
+	// 		s1[k] = s1[i];
+	// 		k++;
+	// }
 }
+
+// void squeeze(char s1[],char s2[])
+// {
+// 	int i, j, k;
+// 	k = 0;
+//
+// 	for(i = 0; s1[i] != '\0';++i)
+// 	{
+// 		for(j = 0; (s1[i] != s2[j]) && s2[j] != '\0'; ++j)
+// 		;
+// 		if(s2[j] == '\0')
+// 		s1[k++] = s1[i];
+// 	}
+//
+// 	s1[k] = '\0';
+// }
