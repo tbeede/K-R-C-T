41e22e53b0a1708de6db9bdd6bc3059faeba2093
gardncl <gardncl@gmail.com>
2017-10-04 21:33:14 -0400

    chapter 5

---
+++ chapter-five/pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void increment(int *ptr) {
+	&ptr = malloc(1 * sizeof(int));
+	ptr* = 8;
+}
+
+int main() {
+	int array1[5] = {1000, 2, 3, 7, 50};
+	int* array2[5] = {1000, 2, 3, 7, 50};
+	increment(array1);
+	return 0;
+}
