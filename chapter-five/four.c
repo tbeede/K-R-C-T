6c6f8fb6b04991543d69d9b2835fd8a820a647ca
tbeede <otherhuman@Nulls-MBP.attlocal.net>
2017-10-11 20:21:44 -0400

    Solution for four

--- chapter-five/four.c
+++ chapter-five/four.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 /*
  * Exercise 5-4 of The C Programming Language
  *
@@ -7,7 +7,24 @@
  * occurs at the end of the string s, and zero otherwise.
 */
 
+int strend(char *s, char *t);
+
 int main(int argc, char **argv)
 {
+	char* s = argv[1];
+	char* t = argv[2];
+	printf("%d\n", strend(s, t));
 	return 0;
+}
+
+int strend(char *s, char *t)
+{
+	s += strlen(s) - 1;
+	t += strlen(t) - 1;
+	if (*s == *t) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
