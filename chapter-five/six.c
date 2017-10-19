0478593eae0bcba26250e79fa25c9fa10134366d
gardncl <gardncl@gmail.com>
2017-10-18 21:12:47 -0400

    solve 5.6 and added all of the pointers forever

--- chapter-five/six.c
+++ chapter-five/six.c
@@ -14,7 +14,7 @@
  * We will use reverse from chapter 3.
  */
 
-void reverse(char *s[]);
+void reverse(char *s);
 // void reverseHelper(char s[], int i, int j);
 void reverseHelper(char *s[], int *i, int j);
 
@@ -24,25 +24,42 @@ void swap(char s[], int i, int j);
 
 int main(int argc, char **argv)
 {
+	printf("hello");
 	char *s = argv[1];
+	printf("REVERSE ME!!! %s\n", s);
+
 	reverse(&s);
 	printf("REVERSE ME!!! %s\n", s);
 	return 0;
 }
-
-void reverse(char *s[])
+//100,000 -> 10101001  
+//(100,000+1 == 100,001) -> 01010100  
+//(100,000+2) -> 10101010   
+//(100,000+3) -> 10101001  
+//(100,000+4) -> 10101101
+//string: abcde
+//memory address in bytes (n) -> a
+//memory address in bytes (n + 1) -> b
+//memory address in bytes (n + 2) -> c
+//memory address in bytes (n + 3) -> d
+//memory address in bytes (n + 4) -> e
+//abcde
+//abcdef
+//s is at memory address n
+void reverse(char *s)
 {
-	reverseHelper(s, 0, strlen(*s) - 1);
-}
+	int offset = strlen(s) - 1
+	char *p1 = s;
+	char *p2 = s + offset; //<- memory address
 
-void reverseHelper(char *s[], int *i, int j)
-{
-	if (i >= j) {
-		return;
+	while(p1 < p2) {
+		char *stow;
+
+		stow = *p1;
+		*p1++ = *p2;
+		*p2-- = stow;
 	}
 
-	swap(s, i, j);
-	reverseHelper(s, i + 1, j);
 }
 
 /* original swap */
@@ -54,12 +71,3 @@ void swap(char s[], int i, int j) {
 	s[j] = stow;
 }
 
-/* the uglier pointer swap */
-// void swap(char *s[], int *i, int *j)
-// {
-// 	char stow;
-//
-// 	stow = *s[*i];
-// 	*s[*i] = *s[*j];
-// 	*s[*j] = stow;
-// }
